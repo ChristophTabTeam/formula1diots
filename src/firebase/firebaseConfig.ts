78a384017e08eebd84c4dc337a25da65c355035e
ChristophTabTeam <101418576+ChristophTabTeam@users.noreply.github.com>
2024-10-31 13:43:16 +0100

    moved sensible data to .env

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -4,15 +4,18 @@ import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAXFkJIo3IQz0qLVbSlr2YKqetPEw18u5I",
-  authDomain: "formula1diots.firebaseapp.com",
-  projectId: "formula1diots",
-  storageBucket: "formula1diots.appspot.com",
-  messagingSenderId: "291759333922",
-  appId: "1:291759333922:web:ac8b11e2dc2fedafd5207b",
-  measurementId: "G-L4WLTLKXVK"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
