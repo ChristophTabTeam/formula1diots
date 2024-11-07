2be63466e1c96b211910025ff7ed857b55603c96
ChristophTabTeam <101418576+ChristophTabTeam@users.noreply.github.com>
2024-11-07 15:06:41 +0100

    hotfix darkmode value save in firestore

--- src/context/darkModeContext/index.tsx
+++ src/context/darkModeContext/index.tsx
@@ -4,20 +4,19 @@ import { doc, updateDoc } from "firebase/firestore";
 import { db } from "../../firebase/firebaseConfig";
 
 // DarkModeProvider-Komponente
-export const DarkModeProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+export const DarkModeProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
   const [isDarkMode, setIsDarkMode] = useState<boolean>(() => {
     // Initialisiere mit dem Wert aus dem localStorage, falls vorhanden
     return localStorage.getItem("darkMode") === "true";
   });
 
   const toggleDarkMode = async (driverId: string) => {
-    setIsDarkMode((prevMode) => {
-      const newMode = !prevMode;
-      localStorage.setItem("darkMode", newMode.toString());
-      return newMode;
-    });
+    const newMode = !isDarkMode;
     const driverDocRef = doc(db, "drivers", driverId);
-      await updateDoc(driverDocRef, { darkMode: isDarkMode });
+    await updateDoc(driverDocRef, { darkMode: newMode });
+    setIsDarkMode(newMode);
   };
 
   useEffect(() => {
